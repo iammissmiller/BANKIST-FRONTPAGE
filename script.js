630489a9176e4980213598c48cfea20690faaeae
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-10-25 02:11:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,101 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+
+/////////
+////scrolling
+////////
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+    section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
+/////page navigation/////
+/////////////////////////
+
+document.querySelector('.nav__links').addEventListener('click' , function(e) {
+
+  e.preventDefault();
+
+  //matching stratergy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+})
+
+
+////////////////////////////////
+////////Tabbed Components///////
+///////////////////////////////
+
+
+const tabs = document.querySelectorAll('operation__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e) {
+  const clicked = e.target.closest('.operations__tab'); 
+
+  console.log(clicked);
+  
+  if (!clicked) return;
+
+  //remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //activate tab
+  clicked.classList.add('operations__tab--active');
+
+  //activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');  
+});
+
+
+//menu fade animation
+const nav = document.querySelector('.nav');
+
+const handleHover = function(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+//passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+///sticky navigation
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+
+});
